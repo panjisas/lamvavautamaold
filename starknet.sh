7d395cab10be2401b12f0c76101c884e5a117c43
panjisas <160022643+panjisas@users.noreply.github.com>
2024-06-10 22:12:21 -0700

    Update starknet.sh

--- starknet.sh
+++ starknet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://rpc.starknet.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://rpc.starknet.lava.build/lava-referer-43404839-4a11-4663-a30b-a4bf5ff75708/"
 
 OUTPUT_DIR="logs"
 
